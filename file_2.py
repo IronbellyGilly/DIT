93aaf9baf59f990e690273d46df56d73f96269a1
Ilnur <fenix717@mail.ru>
2025-02-12 19:37:39 +0300

    сделал 3 задачи

--- file_2.py
+++ file_2.py
@@ -30,4 +30,26 @@ determine_quadrant(0, 0)
 
 # ask_password()
 
-### test
+
+
+def check_string_brackets(input_string):
+    
+    balance = 0
+    for char in input_string:
+        if char == '(':
+            balance += 1 
+        elif char == ')':
+            balance -= 1 
+        if balance < 0:
+            print("NO")
+            return
+    if balance == 0:
+        print("YES")
+    else:
+        print("NO")
+
+check_string_brackets("()")          # Вывод: YES
+check_string_brackets("(()())")      # Вывод: YES
+check_string_brackets("(()(()")      # Вывод: NO
+check_string_brackets(")(")          # Вывод: NO
+check_string_brackets("")             # Вывод: YES
